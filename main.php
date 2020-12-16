9a2788fc6ee4ae25d40524d08683590532ba8367
Hadi374 <ahmadihadi374@gmail.com>
2020-12-16 19:39:05 +0430

    Add basic routing in main.php

--- main.php
+++ main.php
@@ -1,11 +1,25 @@
 <?php
-session_start();
+#session_start();
 
 require_once 'includes/common.php';
 
 $url = explode('/', $_SERVER['REQUEST_URI']);
-echo "Hello";
 
+$action = $url[2];
+echo $action . '<br>';
+$action = explode('?', $action)[0]; // if action contains ? just use first slice for action
+
+switch($action) {
+    case 'register':
+        echo "Registering new User!\n";
+        break;
+    case 'login':
+        echo "Logging In User\n";
+        break;
+    default:
+        echo "Do nothing\n";
+        break;
+}
 
 
 ?>
