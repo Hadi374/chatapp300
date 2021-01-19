ea486620affd717ff46672b6f438b179e0cb3681
Hadi374 <ahmadihadi374@gmail.com>
2021-01-20 00:40:08 +0430

    Add configuration file in includes folder

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'chat');
+define('DB_SERVER', 'localhost');
+define('BASE_URL', 'localhost/projects/chatapp300/');
+define('FILE_PATH', BASE_URL . 'files/');
+
+
+?>
