d38d84b0a4e663853a6868dfb5d89939c041920d
Hadi374 <ahmadihadi374@gmail.com>
2021-02-04 13:53:32 +0430

    connect with common.php file and test some methods

--- includes/common.php
+++ includes/common.php
@@ -2,5 +2,26 @@
 // all codes of backend comes here.
 //$db = new Database;
 
+require_once 'includes/db.php';
+
+class common {
+    private $db;
+
+    function __construct() {
+        echo 'before database';
+        $this->db = new Database;
+    }
+
+    function register($name,  $username, $email, $password) {
+        echo "register " . $name . ' ' . $username . " " . $email . ' ' . $password;
+    }
+
+    function login($username, $password) {
+        echo "login" . $username . ' ' . $password;
+    }
+
+}
+
+
 
 ?>
--- main.php
+++ main.php
@@ -3,18 +3,22 @@
 
 require_once 'includes/common.php';
 
+$app = new common;
+
 $url = explode('/', $_SERVER['REQUEST_URI']);
 
 $action = $url[2];
 echo $action . '<br>';
 $action = explode('?', $action)[0]; // if action contains ? just use first slice for action
 
+
+
 switch($action) {
     case 'register':
-        echo "Registering new User!\n";
+        $app->register('name', 'username', 'email', 'password');
         break;
     case 'login':
-        echo "Logging In User\n";
+        $app->login('name', 'pass');
         break;
     default:
         echo "Do nothing\n";
